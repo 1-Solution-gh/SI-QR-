e312bbd1d7af9b90c7d538c3401c5cabdc9aa81e
samuelbediako <164623026+samuelbediako@users.noreply.github.com>
2025-06-01 21:17:36 -1200

    Update db.php

--- rest/db.php
+++ rest/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "siqr");
+$conn = new mysqli("localhost", "root", "", "siqr")
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
